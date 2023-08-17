3ace756a7363eb7470ea1661aa8e1ab516b0f327
Prakhar <agrawalprakhar893@gmail.com>
2023-08-17 17:08:51 +0530

    Installed Ef Core and setup the db context class

--- DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Program.cs
+++ DbOperationsWithEFCoreApp/DbOperationsWithEFCoreApp/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace DbOperationsWithEFCoreApp
 {
     public class Program
@@ -6,6 +8,10 @@ namespace DbOperationsWithEFCoreApp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"))
+            );
 
             // Add services to the container.
 
